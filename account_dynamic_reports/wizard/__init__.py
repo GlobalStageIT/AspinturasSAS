c7c0f1e0afef002f4ed6deb7400214579295a1ac
@GlobalStageIT <hvargas@globalstageit.com>
2023-05-12 09:03:37 -0500

    ino

---
+++ account_dynamic_reports/wizard/__init__.py
@@ -0,0 +1,6 @@
+from . import general_ledger
+from . import partner_ledger
+from . import trial_balance
+from . import partner_ageing
+from . import financial_report
+from . import trial_balance_partners
