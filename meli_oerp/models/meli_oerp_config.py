e4b69c2f2b1660eaa28fafea4eaab81dbd2d3858
Global Stage IT <hvargas@globalstageit.com>
2025-04-23 11:24:41 -0500

    del meli

--- meli_oerp/models/meli_oerp_config.py
+++
@@ -1,7 +0,0 @@
-#meli_oerp_config
-
-REDIRECT_URI = ''
-ACCESS_TOKEN = ''
-REFRESH_TOKEN = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
--- meli_oerp_multiple/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from . import main
