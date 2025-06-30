a4ffd1300bf133b833309bd975933446d009a39a
GlobalStageIT <63377102+GlobalStageIT@users.noreply.github.com>
2025-06-30 09:59:07 -0500

    Delete l10n_co_payroll directory

--- l10n_co_payroll/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from xmlrpc.client import boolean
-from odoo import models, fields
-
-class ResPartnerBank(models.Model):
-    _inherit = "res.partner"
-
-    is_management = fields.Boolean("Administradora protección social")
-    management_id = fields.Many2one('res.partner.management', string='Administradora')
