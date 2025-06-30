0cf2ef95f0e41d8d49f4750f0c908cb24e9e677d
GlobalStageIT <63377102+GlobalStageIT@users.noreply.github.com>
2025-06-30 15:49:23 -0500

    fix: elimina XML que causaba error, se usará solo hook en Python

--- custom/remove_banner/__manifest__.py
+++ custom/remove_banner/__manifest__.py
@@ -1,13 +1,1 @@
-{
-    'name': 'Remove Banner',
-    'version': '1.0',
-    'summary': 'Desactiva el mensaje de base de pruebas',
-    'category': 'Tools',
-    'author': 'Global Stage IT',
-    'depends': ['web'],
-    'data': [
-        'views/remove_neutralize_banner.xml',
-    ],
-    'installable': True,
-    'application': False,
-}
+'post_init_hook': 'remove_banner',
